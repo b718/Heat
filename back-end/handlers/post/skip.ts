a91df1dfe3a29c1da35e012624ea6a8c1308c24f
whiffy <bryanzhao168@yahoo.ca>
2026-05-02 22:50:19 -0700

    Added simple endpoint to log if the user skipped forwards or backwards

---
+++ back-end/handlers/post/skip.ts
@@ -0,0 +1,11 @@
+import type { Context } from "hono";
+
+import { getLogger } from "../../logger/logger";
+
+const logger = getLogger();
+
+export async function skip(c: Context) {
+	const body = await c.req.json<{ direction: "forward" | "backward" }>();
+	logger.info({ direction: body.direction }, "track skipped");
+	return c.json({ ok: true });
+}
