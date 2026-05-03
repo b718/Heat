2cc199056ad945bec83055a5a0955fafe77932ef
whiffy <bryanzhao168@yahoo.ca>
2026-05-02 22:45:47 -0700

    Added a server function

---
+++ back-end/server/server.ts
@@ -0,0 +1,8 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+export default function startServer() {
+	const server = new Hono();
+	server.use("/*", cors());
+	return server;
+}
