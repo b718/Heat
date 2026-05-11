6548e6ccf8a866323af0e30005e9e0d3f2529d8f
whiffy <bryanzhao168@yahoo.ca>
2026-05-11 16:39:34 -0700

    Removed storer

--- back-end/modules/error/error-storer.ts
+++
@@ -1,9 +0,0 @@
-import { AppError } from "./error";
-
-export class ErrorStorer extends AppError {
-	readonly errorType = "storer";
-
-	constructor(causeOfError: unknown) {
-		super("storer failed", causeOfError);
-	}
-}
--- back-end/modules/storer/storer.ts
+++
@@ -1,6 +0,0 @@
-import type { ParsedResult } from "@heat/types";
-
-export interface Storer {
-	readonly storerType: string;
-	store(result: ParsedResult): Promise<void>;
-}
