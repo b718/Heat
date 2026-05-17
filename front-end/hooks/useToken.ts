48e60c9f165d87cc444cbd7ab7184f2924865592
whiffy <bryanzhao168@yahoo.ca>
2026-05-17 12:02:11 -0700

    Changed useToken hook

--- front-end/hooks/useToken.ts
+++ front-end/hooks/useToken.ts
@@ -1,7 +1,14 @@
+import { useSession } from "@/context/SessionContext";
 import { fetchToken } from "@/services/auth";
 import useSWR from "swr";
 
 export function useToken() {
-	const { data, error, isLoading } = useSWR<string, Error>("fetch-token", fetchToken);
+	const { sessionId } = useSession();
+
+	const { data, error, isLoading } = useSWR<string, Error, [string, string] | null>(
+		sessionId ? ["fetch-token", sessionId] : null,
+		([_, id]) => fetchToken(id),
+	);
+
 	return { data: data ?? "", error, isLoading };
 }
